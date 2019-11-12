d9dcbb77dc0bb063069013211344a64ef838f4a0
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-11-12 20:57:50 +0900

    バリデーションの設定

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,7 @@
 class House < ApplicationRecord
+  validates :name,  presence: true
+  validates :rent,  presence: true
+  validates :adress,  presence: true
+  validates :age,  presence: true
+  validates :content,  presence: true, length: { maximum: 255 }
 end
