998986450e0746d131b0bb1f46ad687ab6ee7899
瀬戸翔太郎 <mutton_omomuki@setoshoutarounoMacBook-Pro.local>
2019-11-12 23:53:53 +0900

    Viewを整頓

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,7 +1,7 @@
 class House < ApplicationRecord
   validates :name,  presence: true
-  validates :rent,  presence: true
+  validates :rent,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0 }
   validates :adress,  presence: true
-  validates :age,  presence: true
-  validates :content,  presence: true, length: { maximum: 255 }
+  validates :age,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0 }
+  validates :content,  presence: true, length: {maximum: 255 }
 end
